43bae5bb4fad47fd68233e2cfb0aeb6568f22403
Drew Boardman <drew.e.boardman@gmail.com>
2019-03-02 21:10:31 -0600

    updated shit

--- src/Reader.hs
+++ src/Reader.hs
@@ -1,7 +1,33 @@
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables          #-}
+
 module Reader (reader) where
 
+import qualified Data.Text as T
 import System.IO
+
+data UserRole = Admin | Pleb
+
+data User = User
+  { first :: String
+  , last :: String
+  , role :: UserRole
+  , password :: String
+  }
+
 
 reader :: FilePath -> IO String
 reader filepath = do
-  file <- openFile filepath ReadMode
+  file <- (readFile filepath :: IO String)
+  let users = (lines file :: [String])
+  undefined
+
+usersmaker :: T.Text -> [User]
+usersmaker row = do
+  _splitted <- T.splitOn "," row
+  return $ User "foo" "bar" Pleb "password"
+
+usermaker2 :: T.Text -> User
+usermaker2 row = user where
+  x = T.splitOn "," row
+  user = User "foo" "bar" Pleb "password"
