816e62b48bc17b25c2f32121b50819b01f3eacd9
James Brechtel <jbrechtel@gmail.com>
2019-03-06 18:39:03 -0500

    Constructs user without using unsafe (!!) operator

--- src/Reader.hs
+++ src/Reader.hs
@@ -5,6 +5,7 @@ module Reader
   ( reader
   ) where
 
+import           Data.Maybe (catMaybes)
 import qualified Data.Text    as T
 import qualified Data.Text.IO as Tio
 import           System.IO
@@ -22,26 +23,21 @@ userRoleFromText _       = Nothing
 data User = User
   { first    :: T.Text
   , last     :: T.Text
-  , role     :: UserRole
   , password :: T.Text
+  , role     :: UserRole
   } deriving (Show)
 
 reader :: FilePath -> IO [User]
 reader filepath = do
   file <- Tio.readFile filepath -- one big Text
   let rows = T.lines file -- split into rows as [Text]
-  let users = [x | Just x <- map usermaker rows]
+  let users = catMaybes $ usermaker <$> rows
   return users
 
 usermaker :: T.Text -> Maybe User
-usermaker row = user
-  where
-    split = T.splitOn "," row -- how do I just put these into the constructor?
-    first' = split !! 0
-    last' = split !! 1
-    role' = userRoleFromText $ split !! 2
-    password' = split !! 3
-    user =
-      case role' of
-        Nothing       -> Nothing
-        Just realRole -> Just $ User first' last' realRole password' -- I'd rather just map over role. How to?
+usermaker row = do
+  case T.splitOn "," row of
+    [firstName, lastName, roleName, pw] ->
+      User firstName lastName pw <$> userRoleFromText roleName
+    _ ->
+      Nothing
